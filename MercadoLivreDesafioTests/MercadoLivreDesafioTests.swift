0c961048311ec20080c1fdcc6a95b64d0c9d5bac
henrique_prudencio_neon <henrique.prudencio@timeneon.com.br>
2019-04-08 18:13:51 -0300

    Added tests Target

---
+++ MercadoLivreDesafioTests/MercadoLivreDesafioTests.swift
@@ -0,0 +1,33 @@
+//
+//  MercadoLivreDesafioTests.swift
+//  MercadoLivreDesafioTests
+//
+//  Created by Henrique Prudêncio on 08/04/19.
+//  Copyright © 2019 Henrique Prudêncio. All rights reserved.
+//
+
+import XCTest
+
+class MercadoLivreDesafioTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
